48d2440193315eff9d9c502cfcabce20ceeea48e
Titou <titouanvenant@gmail.com>
2025-05-05 11:19:09 +0200

    titou

--- serveur.js
+++ serveur.js
@@ -3,3 +3,15 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('accueil', { 
+      logements: logements,
+      title: 'Mini-Airbnb - Accueil' 
+    });
+});
